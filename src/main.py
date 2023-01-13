f47f19665a5330cdd6b5f5536991a1925e4e996a
Graham Waters <graham.waters.business@gmail.com>
2023-01-13 06:16:32 -0600

    background_runner: Updated

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ with open("config/config.json") as f:
 options = webdriver.ChromeOptions()
 options.add_argument("--disable-extensions")
 # options.add_argument('--headless')
-# options.add_argument('--disable-gpu')
+options.add_argument('--disable-gpu')
 options.add_argument("--no-sandbox")
 options.add_argument("start-maximized")
 options.add_argument("disable-infobars")  # <--- Note the option
