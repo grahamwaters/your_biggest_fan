f5b51ee68f2284b5aad74927cbe83bd48f3d9b35
Graham Waters <graham.waters.business@gmail.com>
2023-01-13 08:58:14 -0600

    runs autonomously

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ import datetime
 from datetime import datetime
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from ratelimit import limits, sleep_and_retry
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -51,10 +52,11 @@ driver.get("https://www.github.com/login")
 # password.send_keys(Keys.RETURN)
 
 # wait ten seconds for the follow buttons to load
-# time.sleep(10)
+time.sleep(10)
 
 #* Functions
 
+@sleep_and_retry
 def click_follow_buttons(driver):
     """
     click_follow_buttons
@@ -68,28 +70,40 @@ def click_follow_buttons(driver):
     :return: _description_
     :rtype: _type_
     """
+
     while_counter = 0
     url_clicks = {}
-    while True:
-        if driver.current_url in url_clicks:
-            if url_clicks[driver.current_url] >= 5:
-                continue
+    while True and while_counter < 40:
+        try:
+            if driver.current_url in url_clicks:
+                if url_clicks[driver.current_url] >= 5:
+                    continue
+                else:
+                    url_clicks[driver.current_url] = 0
+            follow_buttons = driver.find_elements(By.CSS_SELECTOR, "input.btn")
+            follow_buttons = [button for button in follow_buttons if button.is_displayed() and button.is_enabled() and button.get_attribute("value") == "Follow" and button.location["y"] < driver.execute_script("return window.innerHeight")]
+            if follow_buttons:
+                follow_buttons[random.randint(0, len(follow_buttons) - 1)].click()
+                print(f"Follow button clicked at {datetime.now()} url: {driver.current_url}")
+                time.sleep(random.randint(6, 10))
+            while_counter += 1
+            if url_clicks[driver.current_url] >= 30:
+                break
+            if while_counter > 30:
+                break
             else:
-                url_clicks[driver.current_url] = 0
-        follow_buttons = driver.find_elements(By.CSS_SELECTOR, "input.btn")
-        follow_buttons = [button for button in follow_buttons if button.is_displayed() and button.is_enabled() and button.get_attribute("value") == "Follow" and button.location["y"] < driver.execute_script("return window.innerHeight")]
-        if follow_buttons:
-            follow_buttons[random.randint(0, len(follow_buttons) - 1)].click()
-            print(f"Follow button clicked at {datetime.now()} url: {driver.current_url}")
-            time.sleep(random.randint(6, 10))
-        if url_clicks[driver.current_url] >= 30 or while_counter >= 30:
-            break
-        if "This repository has no more watchers" in driver.page_source:
-            break
+                while_counter += 1
+            if "This repository has no more watchers" in driver.page_source:
+                break
+            if while_counter > 20:
+                break
+        except Exception as e:
+            print(e)
+            continue
         while_counter += 1
 
 
-
+@sleep_and_retry
 def scrape_for_users(driver):
     """
     scrape_for_users - takes a driver and scrapes the current page for users that are interested in the same things as you are and saves them to a json file in the data folder.
@@ -97,7 +111,7 @@ def scrape_for_users(driver):
     :type driver: _type_
     """
     current_page = driver.current_url
-
+    prev_url = None
     #^ We want to request several pages from github and scrape them for users that are interested in the same things as you are.
     #^ i.e. https://github.com/microsoft/rnx-kit
     # * first scrape - https://github.com/microsoft/rnx-kit/watchers
@@ -127,90 +141,102 @@ def scrape_for_users(driver):
 
         #^ We want to scrape the pages for users that are interested in the same things as you are.
         for url in urls:
-            print(url)
-            # the suffix_page is the page that we are scraping i.e. ?page=2
-            # get the first ten pages
-            baseurl = url
-            time.sleep(random.randint(1, 5))
-            for suffix_page in range(1, 11):
-                time.sleep(random.randint(1, 10))
-                # randomly sleep 10 seconds every 5 pages
-                if suffix_page % 5 == 0:
-                    time.sleep(10)
-                # if the page content contains the text "This repository has no more watchers." then break
-                if "This repository has no more watchers" in driver.page_source:
-                    break
-                url = baseurl + "?page=" + str(suffix_page)
-                #https://github.com/jakevdp/sklearn_tutorial/watchers?page=2
-                # navigate to the page
-                driver.get(url)
-                # wait till the page loads
-                driver.implicitly_wait(10)
+            try:
+                print(url)
+                # the suffix_page is the page that we are scraping i.e. ?page=2
+                # get the first ten pages
+                baseurl = url
+                time.sleep(random.randint(1, 5))
+                for suffix_page in range(1, 11):
+                    count = 0
 
-                #*** ---- Button Clicks ---- ***#
+                    time.sleep(random.randint(1, 10))
+                    # randomly sleep 10 seconds every 5 pages
+                    if suffix_page % 5 == 0:
+                        time.sleep(10)
+                    # if the page content contains the text "This repository has no more watchers." then break
+                    if "This repository has no more watchers" in driver.page_source:
+                        break
+                    url = baseurl + "?page=" + str(suffix_page)
+                    #https://github.com/jakevdp/sklearn_tutorial/watchers?page=2
+                    # navigate to the page
+                    driver.get(url)
+                    # wait till the page loads
+                    #driver.implicitly_wait(10)
+                    if driver.current_url == prev_url:
+                        break
+                    #*** ---- Button Clicks ---- ***#
 
-                #!click_follow_buttons(driver, data) # click the follow buttons
-                #note: could just save the url if the page has a "follow" button and go through those urls later in the while loop below. This would save time and clicks.
-                # save the url if the page has a "follow" button
+                    #!click_follow_buttons(driver, data) # click the follow buttons
+                    #note: could just save the url if the page has a "follow" button and go through those urls later in the while loop below. This would save time and clicks.
+                    # save the url if the page has a "follow" button
 
 
-                #**** ---- Scrape the page for users ---- ****#
-                #^ Get the html of the page
-                try:
-                    page_html = requests.get(url).text
-                except Exception as e:
-                    print("Error getting the page html: ", e)
+                    #**** ---- Scrape the page for users ---- ****#
+                    #^ Get the html of the page
+                    try:
+                        page_html = requests.get(url).text
+                    except Exception as e:
+                        print("Error getting the page html: ", e)
 
-                #^ Save the html to a file in the data folder
-                try:
-                    with open("data/page.html", "w") as f:
-                        f.write(page_html)
-                except Exception as e:
-                    print("Error saving the page html: ", e)
+                    #^ Save the html to a file in the data folder
+                    try:
+                        with open("data/page.html", "w") as f:
+                            f.write(page_html)
+                    except Exception as e:
+                        print("Error saving the page html: ", e)
 
-                #^ Scrape the html for users
+                    #^ Scrape the html for users
 
-                soup = BeautifulSoup(page_html, "html.parser")
-                users = soup.find_all("a", {"class": "Link--primary"})
-                users = [user.text for user in users]
+                    soup = BeautifulSoup(page_html, "html.parser")
+                    users = soup.find_all("a", {"class": "Link--primary"})
+                    users = [user.text for user in users]
 
-                #^ Save the users to the dictionary (json) in the data folder
-                # if the users_dict does not already exist in the data folder then make it
-                if not os.path.exists("data/users_dict.json"):
-                    users_dict = {}
-                else:
-                    with open("data/users_dict.json", "r") as f:
-                        users_dict = json.load(f)
+                    #^ Save the users to the dictionary (json) in the data folder
+                    # if the users_dict does not already exist in the data folder then make it
+                    if not os.path.exists("data/users_dict.json"):
+                        users_dict = {}
+                    else:
+                        with open("data/users_dict.json", "r") as f:
+                            users_dict = json.load(f)
 
-                # get the users from the page
-                # f4 Link--primary
-                # the usernames are between these:
-                # - data-hovercard-type="user" data-hovercard-url="/users/
-                # and
-                # - /hovercard"
-                # i.e. data-hovercard-type="user" data-hovercard-url="/users/username/hovercard"
-                user_pattern = 'data-hovercard-type="user" data-hovercard-url="/users/'
-                users = [] # will hold the usernames
-                if soup is not None:
-                    # look for the pattern
-                    for line in soup:
-                        if user_pattern in line:
-                            # get the username from the line
-                            # the username is between the pattern and the /hovercard"
-                            # i.e. data-hovercard-type="user" data-hovercard-url="/users/username/hovercard"
-                            # so we need to get the index of the pattern and the index of the /hovercard"
-                            # then we can slice the line to get the username
-                            # save the url
-                            viable_urls.append(str(driver.current_url))
-                            pattern_index = line.index(user_pattern)
-                            hovercard_index = line.index("/hovercard")
-                            username = line[pattern_index + len(user_pattern):hovercard_index]
-                            users.append(username) # add the username to the list of users
-                    # update the dictionary of users with the new users and save it back to the csv file
-                    users_dict.update({current_page: users}) # we can extract each user from the list and add them to the dictionary later
-                    with open("data/users_dict.json", "w") as f:
-                        json.dump(users_dict, f)
-                    click_follow_buttons(driver)
+                    # get the users from the page
+                    # f4 Link--primary
+                    # the usernames are between these:
+                    # - data-hovercard-type="user" data-hovercard-url="/users/
+                    # and
+                    # - /hovercard"
+                    # i.e. data-hovercard-type="user" data-hovercard-url="/users/username/hovercard"
+                    user_pattern = 'data-hovercard-type="user" data-hovercard-url="/users/'
+                    users = [] # will hold the usernames
+                    if soup is not None:
+                        # look for the pattern
+                        for line in soup:
+                            if user_pattern in line:
+                                # get the username from the line
+                                # the username is between the pattern and the /hovercard"
+                                # i.e. data-hovercard-type="user" data-hovercard-url="/users/username/hovercard"
+                                # so we need to get the index of the pattern and the index of the /hovercard"
+                                # then we can slice the line to get the username
+                                # save the url
+                                viable_urls.append(str(driver.current_url))
+                                pattern_index = line.index(user_pattern)
+                                hovercard_index = line.index("/hovercard")
+                                username = line[pattern_index + len(user_pattern):hovercard_index]
+                                users.append(username) # add the username to the list of users
+                        # update the dictionary of users with the new users and save it back to the csv file
+                        users_dict.update({current_page: users}) # we can extract each user from the list and add them to the dictionary later
+                        with open("data/users_dict.json", "w") as f:
+                            json.dump(users_dict, f)
+                        click_follow_buttons(driver)
+                        # if th url has not changed for five iterations then break
+                        prev_url = driver.current_url
+                        if url == driver.current_url:
+                            count += 1
+                            if count == 5:
+                                break
+            except Exception as e:
+                print("Error scraping the page: ", e)
         # step 5
         try:
             os.remove("data/page.html")
